a45161db06675475edb5a3e2e4d107377fac780d
yarikroterdamskiygmailcom <37020321+yarikroterdamskiygmailcom@users.noreply.github.com>
2018-07-09 10:25:57 +0300

    'my_fix3'

---
+++ src/componets/firstpage.vue
@@ -0,0 +1,93 @@
+<template >
+    <div id="first">
+        <div class="haed" style="background-image: url('src/assets/foto-fon-2-5.jpg')">
+            <img src="src/assets/logo.png" id="logo" width="200px" height="200px">
+            <div class="demo" >FOLLOW ME</div>
+        </div>
+
+        <div class="middle">
+            <a href="" v-on:click="regs">Registration</a>
+            <a href="" v-on:click="log">Login</a>
+        </div>
+        <globFooter></globFooter>
+    </div>
+</template>
+<script>
+    import GFooter from './GloballFooter.vue'
+    export default {
+        name: 'first',
+        data:function(){
+            return{
+
+            }
+        },
+        methods:{
+            log:function(event){
+                this.$router.push({ path: 'login' })
+                event.preventDefault()
+            },
+            regs:function(event){
+                this.$router.push({ path: 'registration' })
+                event.preventDefault()
+            }
+        },
+        components:{
+            globFooter:GFooter,
+
+        }
+    }
+</script>
+<style>
+    /*название сайта*/
+    .demo {
+        margin: 1em 0 0.5em 0;
+        color: #343434;
+        font-weight: bold;
+        font-family: 'Ultra', sans-serif;
+        font-size: 86px;
+        line-height: 92px;
+        text-transform: uppercase;
+        text-shadow: 0 2px white, 0 3px #777;
+        position:absolute;
+        left: 350px;
+        top:10px;
+
+    }
+
+    #logo{
+        position: relative;
+        left:15px;
+        top:15px;
+
+    }
+
+
+    /* кнопки */
+    .middle  a {
+        text-decoration: none;
+        outline: none;
+        display: inline-block;
+        padding: 12px 40px;
+        margin: 10px 20px;
+        margin-bottom: 235px;
+        border-radius: 80px;
+        background-image: linear-gradient(45deg, #6ab1d7 0%, #33d9de 50%, #002878 100%);
+        background-position: 100% 0;
+        background-size: 200% 200%;
+        font-family: 'Montserrat', sans-serif;
+        font-size: 64px;
+        font-weight: 600;
+        color: white;
+        box-shadow: 0 16px 32px 0 rgba(0,40,120,.35);
+        transition: .5s;
+        position:relative;
+        left:230px;
+        top:90px;
+
+    }
+    .middle a:hover {
+        box-shadow: 0 0 0 0 rgba(0,40,120,0);
+        background-position: 0 0;
+    }
+</style>
+
---
+++ src/componets/profil/navbarProfil.vue
@@ -0,0 +1,170 @@
+<template>
+    <div id="navbar">
+        <div class="globalMyNavBar">
+            <div class="globalMyNavBar_head">
+                <div class="logoAndName">
+                    <img src="src/assets/logo.png" id="logo" width="50px" height="50px">
+                    <span class="logoAndName_name">Follow ME</span>
+                </div>
+
+                <nav class="globalMyNavBar_head_nav">
+                    <a href="#">Home</a>
+                    <a href="#">friends</a>
+                    <a href="#">My albums</a>
+                    <a href="#">News</a>
+                    <a href="#">Settings</a>
+                </nav>
+                <div class="globalMyNavBar_head_searchAndOut">
+                    <form>
+                    <input type="search" style="margin-right: 20px;" class="search">
+                    <input type="submit" value="Search">
+                    <a href="#" @click="LogOut">Logout</a></form>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style>
+    #navbar{
+        margin-left: auto;margin-right: auto;
+        width: 100%;
+    }
+    .globalMyNavBar{
+        background: rgba(0, 153, 255);
+
+
+    }
+    .globalMyNavBar_head{
+        margin-top: 0px;
+        width: 1340px;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 10px;
+        height: 30px;
+    }
+    #logo{
+        position: relative;
+        left:-10px;
+        top:-10px;
+        margin-left: 0px;
+
+    }
+    .logoAndName{
+        font-family: 'Galada', cursive;
+        float: left;
+        margin-top: 0px;
+
+
+    }
+    .logoAndName_name{
+        position: relative;
+        left:40px;
+        top:-30px;
+        margin-left: 0px;
+        margin-top: 40px;
+        font-size: 20px;
+        font-weight: bold;
+        margin-right: 70px
+    }
+    .globalMyNavBar_head_nav{
+        float:left;
+        width: 500px;
+        margin-top: 3px;
+        font-family: 'Nunito', sans-serif;
+    }
+
+   .globalMyNavBar_head_nav a
+   {        padding: 12px;
+       text-decoration: none;
+
+       color: white;
+       border-top: 3px solid rgba(0, 153, 255);
+       border-bottom: 3px solid snow;
+       margin-left: -4px;
+   }
+    .globalMyNavBar_head_searchAndOut a{
+        padding: 15px;
+    text-decoration: none;
+        color: black
+    }
+    .globalMyNavBar_head_nav a:hover,
+    .globalMyNavBar_head_searchAndOut a:hover{
+        background:rgba(20, 136, 245);
+        border-bottom: 3px solid blue;
+
+    }
+    .globalMyNavBar_head_searchAndOut{
+        float: right;
+        margin-left: 0px;
+        padding: 15px;
+        margin-top: -10px;
+    }
+    .clear{
+        clear: both;
+    }
+    .search {
+
+    }
+    input[type="search"] {
+        background-image: url(https://icons8.com/iconizer/files/Token/orig/Search.png);
+        background-repeat: no-repeat; /*Убираем повтор изображения*/
+        background-position: 4px; /*Позиционируем*/
+        background-size: 15px;
+        padding: 10px;
+        outline: none; /*убираем стандартную обводку браузера*/
+        -moz-border-radius: 5px; /*закругляем углы для Mozilla*/
+        -webkit-border-radius: 5px; /*закругляем углы для Chrome, Safari*/
+        border-radius: 5px; /*закругляем углы для остальных браузеров*/
+        padding-left: 20px; /*отступ слева от ввода, чтобы текст не был на картинке(выбирать по размеру картинки)*/
+        height: 20px; /*высота строки ввода*/
+        font-size: 13px; /*размер шрифта*/
+    }
+    input[type="submit"]{
+        padding: 5px;
+        margin-top: -5px;
+        font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+        font-weight: 400;
+        font-size: 14px;
+        color: #9d9e9e;
+        text-shadow: 1px 1px 0 rgba(256,256,256,1.0);
+        background: #fff;
+        border: 1px solid #fff;
+        border-radius: 5px;
+        box-shadow: inset 0 1px 3px rgba(0,0,0,0.50);
+        -moz-box-shadow: inset 0 1px 3px rgba(0,0,0,0.50);
+        -webkit-box-shadow: inset 0 1px 3px rgba(0,0,0,0.50);
+    }
+    input[type="submit"]:hover{
+        background: #dfe9ec;
+        color: #414848;
+    }
+    input[type="submit"]:focus{
+        background: #dfe9ec;
+        color: #414848;
+        box-shadow: inset 0 1px 2px rgba(0,0,0,0.25);
+        -moz-box-shadow: inset 0 1px 2px rgba(0,0,0,0.25);
+        -webkit-box-shadow: inset 0 1px 2px rgba(0,0,0,0.25);
+    }
+</style>
+<script>
+    export default {
+
+        data () {
+            return {}
+
+        },
+        methods:{
+            LogOut:function(){
+                let logOut = confirm('Confirm the action');
+                if(logOut == true){
+                    document.cookie = 'token=';
+                    localStorage.removeItem('id')
+                    this.$router.push({name: 'login'})
+                }
+            }
+        }
+    }
+</script>
